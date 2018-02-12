e69ab0dbf2246c87a1971425c3e5e87156e9f7a7
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2018-02-12 23:50:44 +0700

    Add custom validator example

--- example/command/echo_prompt.go
+++ example/command/echo_prompt.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/jawher/mow.cli"
 	"github.com/ncrypthic/gocli"
@@ -15,13 +16,23 @@ type echoMessage struct {
 	email string
 }
 
+type emailValidator struct{}
+
+func (v *emailValidator) Validate(d []byte) (valid bool, msg string) {
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	if valid = re.Match(d); !valid {
+		msg = fmt.Sprintf("`%s` is not a valid email\n", d)
+	}
+	return valid, msg
+}
+
 func promptHandler(cmd *cli.Cmd) {
 	cmd.Action = func() {
 		f := form.NewForm()
 		m := echoMessage{}
 		fields := []field.Field{
 			field.NewString(&m.name, "name", "Enter your name: "),
-			field.NewString(&m.email, "email", "Enter your email: "),
+			field.WithValidator(field.NewString(&m.email, "email", "Enter your email: "), &emailValidator{}),
 		}
 		f.Prompt(os.Stdout, os.Stdin, fields)
 		fmt.Printf("%v\n", m)
